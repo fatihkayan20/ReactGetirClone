ea2519c141f6012a2529fafaa12146d624a5fc63
fatihkayan20 <fatih.kayan83@gmail.com>
2021-10-05 19:18:20 +0200

    Move mock api to postman

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
 
-axios.defaults.baseURL = "https://testapi.io/api/slaprance/api";
+axios.defaults.baseURL =
+  "https://e8ded1e8-aad1-4586-a497-fe7d7d25cd1b.mock.pstmn.io/api";
 
 function App() {
   return (
